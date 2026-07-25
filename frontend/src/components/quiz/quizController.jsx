8351d34769154b027c856b0c2b3386d31d39429e
supriyavasanth <supriya.vasanthakumaran@mail.utoronto.ca>
2026-07-25 12:57:38 -0400

    remove previous button

--- frontend/src/components/quiz/quizController.jsx
+++ frontend/src/components/quiz/quizController.jsx
@@ -1,8 +1,6 @@
 export default function QuizController({ onSubmit, onSkip, disabled }) {
   return (
     <div className="quiz-controls">
-      <button className="secondary-button">← Previous</button>
-
       <button className="secondary-button" onClick={onSkip}>
         Skip
       </button>
